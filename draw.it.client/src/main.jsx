5ac9b9ea4597116e31270da52eaf3cb44088447a
SteponasDominas <steponasdomgmail.com>
2025-09-24 18:25:24 +0300

    Room page sukurimas bei pavydziu pridejimas

--- draw.it.client/src/main.jsx
+++ draw.it.client/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";                 // šablono pradinis puslapis (neliečiam)
+import RoomPage from "./pages/RoomPage.jsx"; // mūsų naujas papildomas puslapis
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* PAGRINDINIS ŠABLONO PUSLAPIS – paliekam kaip buvo */}
+        <Route path="/" element={<App />} />
+
+        {/* PAPILDOMAS PUSLAPIS – TIK PER URL */}
+        <Route path="/room/:roomId" element={<RoomPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
