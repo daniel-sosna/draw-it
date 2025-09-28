3b6f6fa8acf310270f21d7502204e6230498b607
Laimis Papinigis <laimis.papinigis@mif.stud.vu.lt>
2025-09-28 22:29:46 +0300

    Move Room and RoomSettings to Models.

---
+++ Draw.it.Server/Models/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace Draw.it.Server.Models
+{
+    public class Room
+    {
+        public string Id { get; set; }
+        public RoomSettings Settings { get; set; }
+        public List<string> Players { get; set; }
+    }
+}
---
+++ Draw.it.Server/Models/RoomSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Draw.it.Server.Models
+{
+    public class RoomSettings
+    {
+        public string RoomName { get; set; }
+        public string[] Categories { get; set; }
+        public string[] CustomWords { get; set; }
+        public int DrawingTime { get; set; }
+        public int NumberOfRounds { get; set; }
+    }
+}
