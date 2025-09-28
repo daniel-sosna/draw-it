67759d3c480eed498ffdbe29034f39b9b7dcc82d
Rokas <rdidrikas@gmail.com>
2025-09-28 16:48:02 +0300

    Moved the screen and canvas to correct folders, also added a route for the gameplay screen in app.jsx

--- draw.it.client/src/App.jsx
+++ draw.it.client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Index from "@/pages/index/Index.jsx";
+import GameplayScreen from "@/pages/gameplay/GameplayScreen.jsx";
 import {BrowserRouter, Route, Routes} from "react-router";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Index />}/>
+                <Route path="/gameplay" element={<GameplayScreen />}/>
             </Routes>
         </BrowserRouter>
     )
