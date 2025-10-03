6febaf773af55e5eee0bad90ebdc25baf7c4d896
Rokas <rdidrikas@gmail.com>
2025-10-03 19:26:53 +0300

    feat(gameplay,hub): add a simple GameHub with 3 methods, send message, join, leave

---
+++ Draw.it.Server/Hubs/GameHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Draw.it.Server.Hubs 
+{
+
+    public class GameHub : Hub
+    {
+        public async Task SendMessage(string message, string room, string user) // room might not be a string later, need a separator to update the chats
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+        }
+        
+        public async Task JoinRoom(string room, string user)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("UserJoined", room, Context.ConnectionId, user);
+        }
+        
+        public async Task LeaveRoom(string room, string user)
+        {
+            await  Groups.RemoveFromGroupAsync( Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("UserLeft", room, Context.ConnectionId, user);
+        }
+    }
+}
