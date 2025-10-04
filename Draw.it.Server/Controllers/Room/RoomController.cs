fe3fc41c490553e490195372b327093214b55e69
Laimis Papinigis <laimis.papinigis@mif.stud.vu.lt>
2025-10-04 19:20:48 +0300

    fix(room-controller): Change StartGame endpoint method from POST to PUT

--- Draw.it.Server/Controllers/Room/RoomController.cs
+++ Draw.it.Server/Controllers/Room/RoomController.cs
@@ -2,6 +2,8 @@
 using Draw.it.Server.Models.Room;
 using Draw.it.Server.Services.Room;
 using Draw.it.Server.Controllers.Room.DTO;
+using Draw.it.Server.Exceptions; 
+using Draw.it.Server.Services.User;
 
 namespace Draw.it.Server.Controllers.Room;
 
@@ -11,10 +13,12 @@ public class RoomController : ControllerBase
 {
 
     private readonly IRoomService _roomService;
-
-    public RoomController(IRoomService roomService)
+    private readonly IUserService _userService;
+    
+    public RoomController(IRoomService roomService, IUserService userService)
     {
         _roomService = roomService;
+        _userService = userService;
     }
 
     [HttpPost("generate-id")]
@@ -25,14 +29,57 @@ public class RoomController : ControllerBase
         var response = new GenerateRoomIdResponseDto(roomId);
         return Ok(response);
     }
-
-
-
-    [HttpPost("{roomId}")]
-    public IActionResult CreateRoom([FromRoute] string roomId, [FromBody] RoomSettingsModel settings)
+    
+    [HttpPost("{roomId}/join")]
+    public IActionResult JoinRoom([FromRoute] string roomId, [FromBody] JoinRoomRequestDto request)
     {
-        _roomService.CreateAndAddRoom(roomId, settings);
+        var user = _userService.FindUserById(request.UserId);
+        var room = _roomService.AddPlayerToRoom(roomId, user, request.IsHost);
+        
+        return Ok(room); 
+    }
+    
+    [HttpGet("{roomId}/status")]
+    public IActionResult GetRoomStatus([FromRoute] string roomId)
+    {
+        var room = _roomService.GetRoom(roomId);
+        return Ok(room); 
+    }
+    
+    [HttpPut("{roomId}/player/{userId}/ready")]
+    public IActionResult SetPlayerReadyStatus([FromRoute] string roomId, [FromRoute] long userId, [FromBody] SetReadyStatusRequestDto request)
+    {
+        _roomService.SetPlayerReady(roomId, userId, request.IsReady);
+        return Ok();
+    }
+    
+    [HttpPut("{roomId}/start")]
+    public IActionResult StartGame([FromRoute] string roomId, [FromQuery] long hostUserId)
+    {
+        if (!_roomService.CanStartGame(roomId))
+        {
+            return BadRequest(new { error = "Game cannot be started. Not enough players or not ready." });
+        }
+        
+        var room = _roomService.StartGame(roomId);
+        return Ok(room);
+    }
+    
+    [HttpPut("{roomId}/settings")]
+    public IActionResult UpdateRoomSettings([FromRoute] string roomId, [FromBody] RoomSettingsModel settings)
+    {
+        _roomService.UpdateRoomSettings(roomId, settings);
 
-        return StatusCode(StatusCodes.Status201Created);
+        return Ok(settings);
+    }
+    
+    [HttpPost("join/{roomId}/{userId}")]
+    public IActionResult JoinRoom([FromRoute] string roomId, [FromRoute] long userId)
+    {
+        var user = _userService.FindUserById(userId); 
+        
+        _roomService.JoinRoom(roomId, user); 
+
+        return Ok(); 
     }
 }
