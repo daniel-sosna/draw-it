0efa6a91b89d97a6518f36474f6e2c930b583d48
Daniel Sosna <danik.sosna@gmail.com>
2025-10-05 00:11:20 +0300

    Add logger to SessionService

--- Draw.it.Server/Services/Session/SessionService.cs
+++ Draw.it.Server/Services/Session/SessionService.cs
@@ -5,7 +5,13 @@ namespace Draw.it.Server.Services.Session;
 
 public class SessionService : ISessionService
 {
+    private readonly ILogger<SessionService> _logger;
     private readonly ConcurrentDictionary<string, SessionModel> _sessions = new();
+
+    public SessionService(ILogger<SessionService> logger)
+    {
+        _logger = logger;
+    }
 
     public SessionModel? Get(string sessionId)
     {
@@ -15,6 +21,7 @@ public class SessionService : ISessionService
 
     public void Add(SessionModel session)
     {
+        _logger.LogInformation("Adding session {SessionId} (user: '{UserName}')", session.SessionId, session.UserName);
         _sessions[session.SessionId] = session;
     }
 
