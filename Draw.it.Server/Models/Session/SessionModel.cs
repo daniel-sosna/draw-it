b9dc68d1eedbf1a44fa93939852fd756681ee5b2
Daniel Sosna <danik.sosna@gmail.com>
2025-10-06 19:32:31 +0300

    chore: fix indentation

--- Draw.it.Server/Models/Session/SessionModel.cs
+++ Draw.it.Server/Models/Session/SessionModel.cs
@@ -3,7 +3,7 @@ namespace Draw.it.Server.Models.Session;
 public class SessionModel
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();  // Short-lived identifier
-    public required long UserId { get; set; }    // Link to User
+    public required long UserId { get; set; }   // Link to User
     public string? RoomId { get; set; } // Link to Room
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
--- Draw.it.Server/Services/Room/RoomService.cs
+++ Draw.it.Server/Services/Room/RoomService.cs
@@ -4,107 +4,106 @@ using Draw.it.Server.Models.Session;
 using Draw.it.Server.Repositories.Room;
 using Draw.it.Server.Services.Session;
 
-namespace Draw.it.Server.Services.Room
+namespace Draw.it.Server.Services.Room;
+
+public class RoomService : IRoomService
 {
-    public class RoomService : IRoomService
+    private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    private readonly ILogger<RoomService> _logger;
+    private readonly IRoomRepository _roomRepository;
+    private readonly ISessionService _sessionService;
+
+    public RoomService(ILogger<RoomService> logger, IRoomRepository roomRepository, ISessionService sessionService)
     {
-        private const string Chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        _logger = logger;
+        _roomRepository = roomRepository;
+        _sessionService = sessionService;
+    }
 
-        private readonly ILogger<RoomService> _logger;
-        private readonly IRoomRepository _roomRepository;
-        private readonly ISessionService _sessionService;
+    private string GenerateRandomRoomId()
+    {
+        var random = new Random();
 
-        public RoomService(ILogger<RoomService> logger, IRoomRepository roomRepository, ISessionService sessionService)
+        return new string(Enumerable.Repeat(Chars, 6)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    private string GenerateUniqueRoomId()
+    {
+        string roomId;
+
+        do
         {
-            _logger = logger;
-            _roomRepository = roomRepository;
-            _sessionService = sessionService;
-        }
+            roomId = GenerateRandomRoomId();
+        } while (_roomRepository.ExistsById(roomId));
 
-        private string GenerateRandomRoomId()
+        return roomId;
+    }
+
+    public RoomModel CreateRoom(SessionModel session)
+    {
+        var roomId = GenerateUniqueRoomId();
+        var room = new RoomModel
         {
-            var random = new Random();
+            Id = roomId,
+            HostId = session.UserId,
+            PlayerIds = new List<long> { session.UserId }
+        };
+        _roomRepository.Save(room);
+        _logger.LogInformation("Room with id={roomId} created", roomId);
+        _sessionService.SetRoom(session.Id, roomId);
+        return room;
+    }
 
-            return new string(Enumerable.Repeat(Chars, 6)
-                .Select(s => s[random.Next(s.Length)]).ToArray());
-        }
-
-        private string GenerateUniqueRoomId()
+    public void DeleteRoom(string roomId)
+    {
+        if (!_roomRepository.DeleteById(roomId))
         {
-            string roomId;
-
-            do
-            {
-                roomId = GenerateRandomRoomId();
-            } while (_roomRepository.ExistsById(roomId));
-
-            return roomId;
+            throw new EntityNotFoundException($"Room with id={roomId} not found");
         }
+        // Placeholder!
+        // TODO: Remove roomId from all sessions that had this roomId set
+    }
 
-        public RoomModel CreateRoom(SessionModel session)
+    public RoomModel GetRoom(string roomId)
+    {
+        return _roomRepository.FindById(roomId) ?? throw new EntityNotFoundException($"Room with id={roomId} not found");
+    }
+
+    public void JoinRoom(string roomId, SessionModel session)
+    {
+        var room = _roomRepository.FindById(roomId);
+        if (room == null)
         {
-            var roomId = GenerateUniqueRoomId();
-            var room = new RoomModel
-            {
-                Id = roomId,
-                HostId = session.UserId,
-                PlayerIds = new List<long> { session.UserId }
-            };
-            _roomRepository.Save(room);
-            _logger.LogInformation("Room with id={roomId} created", roomId);
-            _sessionService.SetRoom(session.Id, roomId);
-            return room;
+            throw new EntityNotFoundException($"Room with id={roomId} not found");
         }
-
-        public void DeleteRoom(string roomId)
+        if (room.PlayerIds.Contains(session.UserId))
         {
-            if (!_roomRepository.DeleteById(roomId))
-            {
-                throw new EntityNotFoundException($"Room with id={roomId} not found");
-            }
-            // Placeholder!
-            // TODO: Remove roomId from all sessions that had this roomId set
+            throw new InvalidOperationException($"User with id={session.UserId} is already in the room with id={roomId}");
         }
+        room.PlayerIds.Add(session.UserId);
+        _roomRepository.Save(room);
+        _sessionService.SetRoom(session.Id, roomId);
+    }
 
-        public RoomModel GetRoom(string roomId)
+    public void LeaveRoom(string roomId, SessionModel session)
+    {
+        var room = _roomRepository.FindById(roomId);
+        if (room == null)
         {
-            return _roomRepository.FindById(roomId) ?? throw new EntityNotFoundException($"Room with id={roomId} not found");
+            throw new EntityNotFoundException($"Room with id={roomId} not found");
         }
-
-        public void JoinRoom(string roomId, SessionModel session)
+        if (!room.PlayerIds.Contains(session.UserId))
         {
-            var room = _roomRepository.FindById(roomId);
-            if (room == null)
-            {
-                throw new EntityNotFoundException($"Room with id={roomId} not found");
-            }
-            if (room.PlayerIds.Contains(session.UserId))
-            {
-                throw new InvalidOperationException($"User with id={session.UserId} is already in the room with id={roomId}");
-            }
-            room.PlayerIds.Add(session.UserId);
-            _roomRepository.Save(room);
-            _sessionService.SetRoom(session.Id, roomId);
+            throw new InvalidOperationException($"User with id={session.UserId} is not in the room with id={roomId}");
         }
-
-        public void LeaveRoom(string roomId, SessionModel session)
+        if (room.HostId == session.UserId)
         {
-            var room = _roomRepository.FindById(roomId);
-            if (room == null)
-            {
-                throw new EntityNotFoundException($"Room with id={roomId} not found");
-            }
-            if (!room.PlayerIds.Contains(session.UserId))
-            {
-                throw new InvalidOperationException($"User with id={session.UserId} is not in the room with id={roomId}");
-            }
-            if (room.HostId == session.UserId)
-            {
-                throw new InvalidOperationException("Host cannot leave the room. Consider deleting the room instead.");
-            }
-            room.PlayerIds.Remove(session.UserId);
-            _roomRepository.Save(room);
-            _sessionService.SetRoom(session.Id, null);
+            throw new InvalidOperationException("Host cannot leave the room. Consider deleting the room instead.");
         }
+        room.PlayerIds.Remove(session.UserId);
+        _roomRepository.Save(room);
+        _sessionService.SetRoom(session.Id, null);
     }
 }
