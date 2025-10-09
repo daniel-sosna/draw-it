a1dc4c38359ae5fd1c7e1bc3c766fa550f7b9d43
Laimis Papinigis <laimis.papinigis@mif.stud.vu.lt>
2025-10-09 08:37:17 +0300

    Add Category model

--- Draw.it.Server/Models/Room/RoomSettingsModel.cs
+++ Draw.it.Server/Models/Room/RoomSettingsModel.cs
@@ -3,7 +3,7 @@
     public class RoomSettingsModel
     {
         public string RoomName { get; set; } = string.Empty;
-        public string[] Categories { get; set; } = Array.Empty<string>();
+        public long CategoryId { get; set; }
         public string[] CustomWords { get; set; } = Array.Empty<string>();
         public int DrawingTime { get; set; } = 60;
         public int NumberOfRounds { get; set; } = 3;
