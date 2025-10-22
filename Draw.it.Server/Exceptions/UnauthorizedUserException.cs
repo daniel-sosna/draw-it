ee114fb88ef24cb0f5fe8c1e0645b3b48843b362
stitas <Fakemaskw@gmail.com>
2025-10-22 21:37:25 +0300

    Add optional message

--- Draw.it.Server/Exceptions/UnauthorizedUserException.cs
+++ Draw.it.Server/Exceptions/UnauthorizedUserException.cs
@@ -4,5 +4,5 @@ namespace Draw.it.Server.Exceptions;
 
 public class UnauthorizedUserException : AppException
 {
-    public UnauthorizedUserException(string message) : base(message, HttpStatusCode.Unauthorized) { }
+    public UnauthorizedUserException(string message = "User ID claim missing.") : base(message, HttpStatusCode.Unauthorized) { }
 }
--- Draw.it.Server/Extensions/HttpContextExtension.cs
+++ Draw.it.Server/Extensions/HttpContextExtension.cs
@@ -10,7 +10,7 @@ public static class HttpContextExtension
     // Extension method to get current user's ID from claims in HttpContext
     public static long ResolveUserId(this HttpContext context)
     {
-        var stringUserId = (context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value) ?? throw new UnauthorizedUserException("User ID claim missing.");
+        var stringUserId = (context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value) ?? throw new UnauthorizedUserException();
 
         if (!long.TryParse(stringUserId, out long userId))
         {
