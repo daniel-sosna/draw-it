8d0342668e0954236b48525e5d8d571abea9ee91
Laimis Papinigis <laimis.papinigis@mif.stud.vu.lt>
2025-11-03 23:04:31 +0200

    feat(model): Introduce GameModel for game session management

---
+++ Draw.it.Server/Models/Game/GameModel.cs
@@ -0,0 +1,18 @@
+﻿using Draw.it.Server.Enums;
+
+namespace Draw.it.Server.Models.Game;
+
+public class GameModel
+{
+    public string RoomId { get; set; }
+    public RoomStatus Status { get; set; } = RoomStatus.InGame;
+    public int TotalRounds { get; set; }
+    public int CurrentRound { get; set; } = 0;
+    public long CurrentDrawerId { get; set; }
+    public List<long> TurnOrder { get; set; } = new List<long>();
+    public int CurrentTurnIndex { get; set; } = 0;
+    public int TurnDuration { get; set; }
+    public int RemainingSeconds { get; set; }
+    public string WordToDraw { get; set; } = string.Empty;
+    public List<string> GuessedPlayersIds { get; set; } = new List<string>();
+}
