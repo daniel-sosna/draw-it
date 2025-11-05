b0824a9fa580aaa932207822f48de3dd69263bb4
Rokas <rdidrikas@gmail.com>
2025-11-05 13:25:39 +0200

    add the gameplay connection for word showing when the game service is complete

--- draw.it.client/src/components/gameplay/WordComponent.jsx
+++ draw.it.client/src/components/gameplay/WordComponent.jsx
@@ -1,14 +1,35 @@
 ﻿import React, { useState } from "react";
+import {GameplayHubContext} from "@/utils/GameplayHubProvider.jsx";
 export default function WordComponent() {
 
-    const [canvasText, setCanvasText] = useState("");
+    const [word, setWord] = useState("");
+    const gameplayConnection = useContext(GameplayHubContext);
 
+    /*
+    useEffect(() => {
+        if (!gameplayConnection) return;
+
+        gameplayConnection.on("ReceiveWordToDraw", (word) => {
+            setWord(word); // set word for drawer
+        });
+
+        gameplayConnection.on("RoundStarted", () => {
+            setWord(""); // clear word for guesser
+        });
+
+        return () => {
+            gameplayConnection.off("ReceiveWordToDraw");
+            gameplayConnection.off("RoundStarted");
+        };
+    }, [gameplayConnection]);
+    */
+    
     return (
         //{/* Word to draw/guess */}
         <div className="flex items-center justify-center mb-2">
             <div className="text-center">
                 <div className="text-2xl font-bold text-gray-800 bg-white px-4 py-1 rounded-lg border-2 border-orange-200 shadow-sm">
-                    {canvasText || "Waiting for word..."}
+                    {word || "Waiting for word..."}
                 </div>
             </div>
         </div>
