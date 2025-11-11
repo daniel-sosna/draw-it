d0f2149fb5bbcb85c50836ecab098d595c1d3618
stitas <Fakemaskw@gmail.com>
2025-11-11 14:47:40 +0200

    Update room repo to use var

--- draw-it.Server.Tests/Repositories/Room/InMemRoomRepositoryTest.cs
+++ draw-it.Server.Tests/Repositories/Room/InMemRoomRepositoryTest.cs
@@ -19,10 +19,10 @@ public class InMemRoomRepositoryTest
     [Test]
     public void whenSaveRoom_thenRoomCanBeFoundById()
     {
-        RoomModel room = new RoomModel{ Id = RoomId, HostId = HostId };
+        var room = new RoomModel{ Id = RoomId, HostId = HostId };
 
         _repository.Save(room);
-        RoomModel? result = _repository.FindById(RoomId);
+        var result = _repository.FindById(RoomId);
 
         Assert.That(result, Is.Not.Null);
         Assert.That(result, Is.EqualTo(room));
@@ -31,11 +31,11 @@ public class InMemRoomRepositoryTest
     [Test]
     public void whenSaveRoom_thenExistsByIdReturnsTrue()
     {
-        RoomModel room = new RoomModel{ Id = RoomId, HostId = HostId };
+        var room = new RoomModel{ Id = RoomId, HostId = HostId };
 
         _repository.Save(room);
 
-        bool exists = _repository.ExistsById(RoomId);
+        var exists = _repository.ExistsById(RoomId);
 
         Assert.That(exists, Is.True);
     }
@@ -43,7 +43,7 @@ public class InMemRoomRepositoryTest
     [Test]
     public void whenRoomNotSaved_thenExistsByIdReturnsFalse()
     {
-        bool exists = _repository.ExistsById(RoomId);
+        var exists = _repository.ExistsById(RoomId);
 
         Assert.That(exists, Is.False);
     }
@@ -51,10 +51,10 @@ public class InMemRoomRepositoryTest
     [Test]
     public void whenDeleteExistingRoom_thenReturnsTrueAndRoomIsRemoved()
     {
-        RoomModel room = new RoomModel{ Id = RoomId, HostId = HostId };
+        var room = new RoomModel{ Id = RoomId, HostId = HostId };
         _repository.Save(room);
 
-        bool deleted = _repository.DeleteById(RoomId);
+        var deleted = _repository.DeleteById(RoomId);
 
         Assert.That(deleted, Is.True);
         Assert.That(_repository.FindById(RoomId), Is.Null);
@@ -64,7 +64,7 @@ public class InMemRoomRepositoryTest
     [Test]
     public void whenDeleteNonExistingRoom_thenReturnsFalse()
     {
-        bool deleted = _repository.DeleteById(RoomId);
+        var deleted = _repository.DeleteById(RoomId);
 
         Assert.That(deleted, Is.False);
     }
@@ -72,15 +72,15 @@ public class InMemRoomRepositoryTest
     [Test]
     public void whenMultipleRoomsSaved_thenGetAllReturnsAllRooms()
     {
-        RoomModel room1 = new RoomModel{ Id = RoomId, HostId = HostId };
-        RoomModel room2 = new RoomModel { Id = AnotherRoomId, HostId = HostId };
+        var room1 = new RoomModel{ Id = RoomId, HostId = HostId };
+        var room2 = new RoomModel{ Id = AnotherRoomId, HostId = HostId };
 
         _repository.Save(room1);
         _repository.Save(room2);
 
         var rooms = _repository.GetAll().ToList();
 
-        Assert.That(rooms.Count, Is.EqualTo(2));
+        Assert.That(rooms, Has.Count.EqualTo(2));
         Assert.That(rooms, Does.Contain(room1));
         Assert.That(rooms, Does.Contain(room2));
     }
