bb4741715240d3975c900d006973fab8743529fa
stitas <Fakemaskw@gmail.com>
2025-11-13 22:49:48 +0200

    Add user controller test

---
+++ draw-it.Server.Tests/Controllers/UserControllerTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+using System.Security.Claims;
+using Draw.it.Server.Controllers.User;
+using Draw.it.Server.Controllers.User.DTO;
+using Draw.it.Server.Services.User;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace draw_it.Tests.Controllers;
+
+public class UserControllerTest
+{
+    private const long UserId = 1;
+    private const string NewName = "NEW_NAME";
+
+    private UserController _userController;
+    private DefaultHttpContext _httpContext;
+    private Mock<IUserService> _userService = new();
+
+    [SetUp]
+    public void Setup()
+    {
+        _userService = new Mock<IUserService>();
+        _httpContext = new DefaultHttpContext();
+
+        _userController = new UserController(_userService.Object)
+        {
+            ControllerContext = new ControllerContext
+            {
+                HttpContext = _httpContext
+            }
+        };
+    }
+
+    [Test]
+    public void whenUpdateName_thenServiceCalledAndNoContentReturned()
+    {
+        SetupAuthenticatedUser();
+        var request = new UpdateNameRequestDto(NewName);
+
+        var result = _userController.UpdateName(request);
+
+        Assert.That(result, Is.InstanceOf<NoContentResult>());
+        _userService.Verify(s => s.UpdateName(UserId, NewName), Times.Once);
+    }
+
+    [Test]
+    public void whenUserControllerClass_thenHasAuthorizeAttribute()
+    {
+        var attr = typeof(UserController).GetCustomAttribute<AuthorizeAttribute>();
+        Assert.That(attr, Is.Not.Null);
+    }
+
+    [Test]
+    public void whenUpdateName_thenHasHttpPostAttributeWithNewNameRoute()
+    {
+        var method = typeof(UserController).GetMethod(nameof(UserController.UpdateName));
+        Assert.That(method, Is.Not.Null);
+
+        var httpPostAttr = method!.GetCustomAttribute<HttpPostAttribute>();
+        Assert.That(httpPostAttr, Is.Not.Null);
+        Assert.That(httpPostAttr!.Template, Is.EqualTo("new-name"));
+    }
+    
+    private void SetupAuthenticatedUser()
+    {
+        var identity = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, UserId.ToString())
+        });
+
+        _httpContext.User = new ClaimsPrincipal(identity);
+    }
+}
